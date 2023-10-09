597bca20e0a0e0c9998fa843b511f16f281fc8a1
Kamil Matuszewski <kmatuszewski2310@gmail.com>
2023-10-09 18:43:21 +0200

    db delete implemented

--- App.jsx
+++ App.jsx
@@ -2,9 +2,8 @@ import React from "react"
 import Sidebar from "./components/Sidebar"
 import Editor from "./components/Editor"
 import Split from "react-split"
-import { nanoid } from "nanoid"
-import { addDoc, onSnapshot } from "firebase/firestore"
-import { notesCollection } from "./firebase"
+import { addDoc, deleteDoc, doc, onSnapshot } from "firebase/firestore"
+import { notesCollection, db } from "./firebase"
 
 export default function App() {
     const [notes, setNotes] = React.useState([]);
@@ -44,10 +43,10 @@ export default function App() {
         })
     }
 
-    function deleteNote(event, noteId) {
-        event.stopPropagation()
-        setNotes(oldNotes => oldNotes.filter(note => note.id !== noteId))
-        setCurrentNoteId(notes[0]?.id);
+    async function deleteNote(noteId) {
+        const docRef =  doc(db, "notes", noteId);
+        await deleteDoc(docRef);
+        // setCurrentNoteId(notes[0]?.id);
     }
 
     return (
